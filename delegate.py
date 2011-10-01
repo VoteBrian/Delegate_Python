1b0e10bfaa923fd5f2394a6157792a2f55d18a02
Brian Flores <bflores@gmail.com>
2011-10-01 00:19:43 -0400

    I got multiple frames working on a single window with much of the styling of the program done.  The side panel has buttons that were pulled in from a database.  I need to work on the styling of the individual tasks and add more information to them besides just the task name.  I also need to be able to set the window size on startup.  None of the buttons work yet.

--- delegate.py
+++ delegate.py
@@ -37,40 +37,83 @@ for row in curs:
 # ---------
 # GUI STUFF
 # ---------
+root = Tk()
+btn_side = range(4)
 
-class GUIFramework(Frame):
+container_Frame = Frame(root)
+container_Frame.config(bg="#e8e8e8", pady=10, padx=2)
+container_Frame.pack(fill=BOTH, expand=1)
 
-    def __init__(self, master=None):
-        Frame.__init__(self, master)
-        self.master.title("Type Some Text")
-        self.config(bg='#e8e8e8')
-        self.grid(padx=10, pady=0)
-        self.pack(expand=YES, fill=BOTH)
-        self.CreateWidgets()
+sidePanel = Frame(container_Frame)
+sidePanel.config(bg="#e8e8e8")
 
-    def CreateWidgets(self):
-        self.lbText = Label(self, text="Sort Options")
-        self.lbText.config(bg='#e8e8e8')
-        self.lbText.grid(row=0, column=0)
-        
-        curs.execute("select name from sort_options ORDER BY '_id'")
-        x = len(curs.fetchall())
-        print x
-        curs.execute("select name from sort_options ORDER BY '_id'")
+curs.execute('select name from sort_options')
+r=0
+for row in curs:
+    btn_side[r] = Button(sidePanel, text=row)
+    btn_side[r].config(anchor=W, relief=FLAT, bg="#e8e8e8")
+    btn_side[r].pack(side=TOP, fill=X)
+
+    div = Frame(sidePanel)
+    div.config(bg="#ffffff", height=1, relief=FLAT)
+    div.pack(fill=X, pady=2, padx=2)
+
+    r = r+1
+
+sidePanel.pack(side=LEFT, fill=BOTH)
 
 
-        btn = range(x)
-        r = 1
-        for row in curs:
-            btn[r-1] = Button(self, text=row)
-            btn[r-1].config(bg='#afcefc', relief=FLAT)
-            btn[r-1].grid(row=r, column=0)
-            #btn[r-1].pack(expand=YES, fill=X)
-            
-            r = r + 1
+main_panel = Frame(container_Frame)
+main_panel.config(bg="#ffffff")
 
-        #btn[0].pack(expand=YES, fill=X)
-            
-if __name__ == "__main__":
-    guiFrame = GUIFramework()
-    guiFrame.mainloop()
+new_task = Button(main_panel, text="+ Create Task")
+new_task.config(bg="#d9d9d9", relief=FLAT)
+new_task.pack(padx=2, pady=2, side=TOP, fill=X)
+
+div = Frame(main_panel)
+div.config(height=2, bd=1, relief=SUNKEN)
+div.pack(fill=X, pady=2, padx=2)
+
+new_task = Checkbutton(main_panel, text="Finish Delegate program")
+new_task.config(bg="#ffffff", anchor=W, relief=FLAT)
+new_task.pack(side=TOP, fill=X)
+
+div = Frame(main_panel)
+div.config(height=2, bd=1, relief=SUNKEN)
+div.pack(fill=X, pady=2, padx=2)
+
+new_task = Checkbutton(main_panel, text="Figure out why your spent time on this")
+new_task.config(bg="#ffffff", anchor=W, relief=FLAT)
+new_task.pack(side=TOP, fill=X)
+
+div = Frame(main_panel)
+div.config(height=2, bd=1, relief=SUNKEN)
+div.pack(fill=X, pady=2, padx=2)
+
+new_task = Checkbutton(main_panel, text="Laundry")
+new_task.config(bg="#ffffff", anchor=W, relief=FLAT)
+new_task.pack(side=TOP, fill=X)
+
+div = Frame(main_panel)
+div.config(height=2, bd=1, relief=SUNKEN)
+div.pack(fill=X, pady=2, padx=2)
+
+new_task = Checkbutton(main_panel, text="Drink at least 6 beers")
+new_task.config(bg="#ffffff", anchor=W, relief=FLAT)
+new_task.pack(side=TOP, fill=X)
+
+div = Frame(main_panel)
+div.config(height=2, bd=1, relief=SUNKEN)
+div.pack(fill=X, pady=2, padx=2)
+
+new_task = Checkbutton(main_panel, text="Nurse hangover")
+new_task.config(bg="#ffffff", anchor=W, relief=FLAT)
+new_task.pack(side=TOP, fill=X)
+
+div = Frame(main_panel)
+div.config(height=2, bd=1, relief=SUNKEN)
+div.pack(fill=X, pady=2, padx=2)
+
+main_panel.pack(side=LEFT, fill=BOTH, expand=1)
+
+root.mainloop()
