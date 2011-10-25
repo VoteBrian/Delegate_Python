bb9f302ea1803abd05243909641edf7b574c6e0b
Brian Flores <bflores@gmail.com>
2011-10-24 23:14:06 -0400

    Added checkbox and >> to the TaskRow.  Currently, neither do anything.  Also, it looks like this broke the alignment of the title and description.

--- TaskRow.py
+++ TaskRow.py
@@ -25,14 +25,31 @@ class TaskRow():
         self.frameRow = Frame(self._frame)
         self.frameRow.config(bg="#d9d9d9")
         self.frameRow.pack(side=TOP, fill=X, expand=0, anchor=N)
+        
 
-        self.frameTitle = Label(self.frameRow, font=("Arial", 12), padx=10)
+        self.titleRow = Frame(self.frameRow)
+        self.titleRow.pack(side=TOP, fill=X, expand=0, anchor=N)
+
+        self.priorityLabel = Label(self.titleRow, font=("Arial", 10))
+        self.priorityLabel.config(bg="#ffffff", text=">>")
+        self.priorityLabel.pack(side=LEFT, fill=BOTH, expand=0)
+        
+        self.frameTitle = Label(self.titleRow, font=("Arial", 12), padx=10)
         self.frameTitle.config(bg="#ffffff", text=self._title, anchor=W)
-        self.frameTitle.pack(side=TOP, fill=X, expand=0)
+        self.frameTitle.pack(side=LEFT, fill=X, expand=1)
+        
 
-        self.frameDesc = Label(self.frameRow, font=("Arial", 8), padx=10)
+        self.descRow = Frame(self.frameRow)
+        self.descRow.pack(side=TOP, fill=X, expand=0, anchor=N)
+
+        self.checkBox = Checkbutton(self.descRow)
+        self.checkBox.config(bg="#ffffff")
+        self.checkBox.pack(side=LEFT, fill=BOTH, expand=0)
+        
+        self.frameDesc = Label(self.descRow, font=("Arial", 8), padx=10)
         self.frameDesc.config(bg="#ffffff", fg="#555555", text=self._desc, anchor=W)
-        self.frameDesc.pack(side=TOP, fill=BOTH, expand=1)
+        self.frameDesc.pack(side=LEFT, fill=BOTH, expand=1)
+        
 
         self.rowBorder = Frame(self.frameRow, padx=10)
         self.rowBorder.config(height=1, relief=FLAT, bg="#cbcbcb")
